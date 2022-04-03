d532dd60e41d9a62365b8fe7135bfc19224699a6
kienpro9a <70696780+kienpro9a@users.noreply.github.com>
2022-04-03 18:37:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,193 @@
-import React from 'react';
 import {
-  ChakraProvider,
+  Avatar,
   Box,
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Icon,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Stack
+} from "@chakra-ui/react";
+import {
+  FiHome,
+  FiTrendingUp,
+  FiCompass,
+  FiStar,
+  FiSettings,
+  FiMenu,
+  FiSearch
+} from "react-icons/fi";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import React from "react";
+import Main from "./components/main";
 
-function App() {
+const LinkItems = [
+  { name: "Home", icon: FiHome },
+  { name: "Trending", icon: FiTrendingUp },
+  { name: "Explore", icon: FiCompass },
+  { name: "Favourites", icon: FiStar },
+  { name: "Settings", icon: FiSettings }
+];
+
+export default function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+    <Box
+      as="section"
+      bg={useColorModeValue("gray.50", "gray.700")}
+      minH="100vh"
+    >
+      <SidebarContent display={{ base: "none", md: "unset" }} />
+      <Drawer isOpen={isOpen} onClose={onClose} placement="left">
+        <DrawerOverlay />
+        <DrawerContent>
+          <SidebarContent w="full" borderRight="none" />
+        </DrawerContent>
+      </Drawer>
+      <Box ml={{ base: 0, md: 60 }} transition=".3s ease">
+        <Flex
+          as="header"
+          align="center"
+          justify="space-between"
+          w="full"
+          px="4"
+          bg={useColorModeValue("white", "gray.800")}
+          borderBottomWidth="1px"
+          borderColor={useColorModeValue("inherit", "gray.700")}
+          h="14"
+        >
+          <IconButton
+            aria-label="Menu"
+            display={{ base: "inline-flex", md: "none" }}
+            onClick={onOpen}
+            icon={<FiMenu />}
+            size="sm"
+          />
+          <InputGroup w="96" display={{ base: "none", md: "flex" }}>
+            <InputLeftElement color="gray.500">
+              <FiSearch />
+            </InputLeftElement>
+            <Input placeholder="Search for articles..." />
+          </InputGroup>
+
+          <Flex align="center">
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}>
+                  <Avatar
+                    size={'sm'}
+                    src={
+                      'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                    }
+                  />
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>Link 1</MenuItem>
+                  <MenuItem>Link 2</MenuItem>
+                  <MenuDivider />
+                  <MenuItem>Link 3</MenuItem>
+                </MenuList>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+
+        <Box as="main" p="4">
+          <Main/>
+        </Box>
       </Box>
-    </ChakraProvider>
+    </Box>
   );
 }
 
-export default App;
+const NavItem = (props) => {
+  const { icon, children, ...rest } = props;
+  return (
+    <Flex
+      align="center"
+      px="4"
+      pl="4"
+      py="3"
+      cursor="pointer"
+      color={useColorModeValue("inherit", "gray.400")}
+      _hover={{
+        bg: useColorModeValue("gray.100", "gray.900"),
+        color: useColorModeValue("gray.900", "gray.200")
+      }}
+      role="group"
+      fontWeight="semibold"
+      transition=".15s ease"
+      {...rest}
+    >
+      {icon && <Icon mx="2" boxSize="4" as={icon} />}
+      {children}
+    </Flex>
+  );
+};
+
+const SidebarContent = (props) => (
+  <Box
+    as="nav"
+    pos="fixed"
+    top="0"
+    left="0"
+    zIndex="sticky"
+    h="full"
+    pb="10"
+    overflowX="hidden"
+    overflowY="auto"
+    bg={useColorModeValue("white", "gray.800")}
+    borderColor={useColorModeValue("inherit", "gray.700")}
+    borderRightWidth="1px"
+    w="60"
+    {...props}
+  >
+    <Flex px="4" py="5" align="center">
+      <Text
+        fontSize="2xl"
+        ml="2"
+        color={useColorModeValue("brand.500", "white")}
+        fontWeight="semibold"
+      >
+        Logo
+      </Text>
+    </Flex>
+    <Flex
+      direction="column"
+      as="nav"
+      fontSize="sm"
+      color="gray.600"
+      aria-label="Main Navigation"
+    >
+      {LinkItems.map((link) => (
+        <NavItem key={link.name} icon={link.icon}>
+          {link.name}
+        </NavItem>
+      ))}
+    </Flex>
+  </Box>
+);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
-import { ColorModeScript } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
